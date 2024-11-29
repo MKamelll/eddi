82efe25c2512fcdaefee814b3b36ac1e5538161c
Mina Kamel <minallkamel@gmail.com>
2024-11-29 14:13:54 +0200

    change tui library

--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.22.2
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/bubbletea v1.2.3 // indirect
-	github.com/charmbracelet/lipgloss v1.0.0 // indirect
 	github.com/charmbracelet/x/ansi v0.4.5 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -19,5 +19,6 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,88 +2,110 @@ package main
 
 import (
 	"log"
-	"strconv"
 
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/gdamore/tcell/v2"
 )
 
 type Editor struct {
-	lines              []string
-	curr_row           int
-	curr_col           int
-	default_cursor     string
-	curr_window_width  int
-	curr_window_height int
+	lines          string
+	curr_row       int
+	curr_col       int
+	default_cursor string
+	width          int
+	height         int
 }
 
 func NewEditor() Editor {
 	return Editor{
-		lines:              []string{},
-		curr_row:           0,
-		curr_col:           0,
-		default_cursor:     "|",
-		curr_window_width:  0,
-		curr_window_height: 0,
+		lines:          "",
+		curr_row:       0,
+		curr_col:       0,
+		default_cursor: "|",
+		width:          0,
+		height:         0,
 	}
-}
-
-func (e Editor) Init() tea.Cmd {
-	return tea.ClearScreen
-}
-
-func (e Editor) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		{
-			switch msg.String() {
-			case "ctrl+q":
-				return e, tea.Quit
-			case "backspace":
-				{
-					if len(e.lines) > 0 {
-						e.lines = e.lines[0 : len(e.lines)-1]
-					}
-
-					break
-				}
-			case "enter":
-				{
-					e.curr_row += 1
-					e.lines = append(e.lines, "\n")
-					e.lines = append(e.lines, strconv.Itoa(e.curr_row+1)+"~ ")
-					break
-				}
-			default:
-				{
-					e.lines = append(e.lines, msg.String())
-					e.curr_col += 1
-					break
-				}
-			}
-		}
-	}
-
-	if len(e.lines) < 1 {
-		e.lines = append(e.lines, "1~ ")
-	}
-
-	return e, nil
-}
-
-func (e Editor) View() string {
-	s := ""
-	for _, str := range e.lines {
-		s += str
-	}
-
-	s += e.default_cursor
-
-	return s
 }
 
 func main() {
-	p := tea.NewProgram(NewEditor())
-	if _, err := p.Run(); err != nil {
-		log.Fatal("EddiError: " + err.Error())
+
+	editor := NewEditor()
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatal("Couldn't create a screen: " + err.Error())
+	}
+
+	defer screen.Fini()
+
+	if err := screen.Init(); err != nil {
+		log.Fatal("Couldn't initialize the screen " + err.Error())
+	}
+
+	style := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
+	screen.SetStyle(style)
+
+	screen.Clear()
+	drawText(screen, editor.curr_col, editor.curr_row, style, editor.default_cursor)
+
+	for {
+
+		ev := screen.PollEvent()
+
+		switch ev := ev.(type) {
+		case *tcell.EventKey:
+			{
+
+				switch ev.Key() {
+				case tcell.KeyCtrlQ:
+					return
+				case tcell.KeyEnter:
+					{
+						editor.curr_row += 1
+						editor.curr_col = 0
+						break
+					}
+				case tcell.KeyBackspace:
+				case tcell.KeyBackspace2:
+					{
+						if editor.curr_col > 0 {
+							editor.curr_col -= 1
+						}
+
+						if len(editor.lines) > 0 {
+
+							editor.lines = editor.lines[0 : len(editor.lines)-1]
+						}
+
+						drawText(screen, 0, editor.curr_row, style, editor.lines)
+						drawText(screen, editor.curr_col, editor.curr_row, style, editor.default_cursor)
+						drawText(screen, editor.curr_col+1, editor.curr_row, style, " ")
+
+						break
+					}
+				default:
+					{
+
+						editor.curr_col += 1
+						editor.lines += string(ev.Rune())
+						drawText(screen, 0, editor.curr_row, style, editor.lines)
+						drawText(screen, editor.curr_col, editor.curr_row, style, editor.default_cursor)
+					}
+				}
+
+				screen.Show()
+
+			}
+		case *tcell.EventResize:
+			{
+				screen.Sync()
+			}
+		}
+
+	}
+
+}
+
+func drawText(screen tcell.Screen, x, y int, style tcell.Style, str string) {
+	for i, c := range str {
+		screen.SetContent(x+i, y, c, nil, style)
 	}
 }
